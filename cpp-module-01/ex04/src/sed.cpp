d53919284ec06613d4ebbb41479b5d77df152380
mlanca-c <marialsottomayor@gmail.com>
2022-06-21 20:03:50 +0100

    ex04 done Fixed argv stupid thing with whitespace characters

--- cpp-module-01/ex04/src/sed.cpp
+++ cpp-module-01/ex04/src/sed.cpp
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 18:13:39 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/06/21 19:36:46 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2022/06/21 20:02:45 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,17 @@
 #include <fstream>
 
 static std::string	sedLine( std::string line, std::string s1, std::string s2 );
+static std::string	argvIsForLosers( std::string str );
+
 
 void	sed( std::string filename, std::string s1, std::string s2 ) {
 
 	std::ifstream	file;
 	std::ofstream	replaceFile;
 	std::string		line;
+
+	s1 = argvIsForLosers( s1 );
+	s2 = argvIsForLosers( s2 );
 
 	file.open( filename );
 	if ( !file.is_open() ) {
@@ -35,7 +40,6 @@ void	sed( std::string filename, std::string s1, std::string s2 ) {
 	}
 
 	while (getline( file, line )) {
-		DEBUG(line);
 		replaceFile << sedLine( line, s1, s2 ) << std::endl;
 	}
 
@@ -59,3 +63,33 @@ static std::string	sedLine( std::string line, std::string s1, std::string s2 ) {
 	return ( line );
 }
 
+static std::string	argvIsForLosers( std::string str ) {
+
+	std::size_t	i;
+	char		c;
+
+	i = 0;
+	while ( 1 ) {
+		if ( (i = str.find("\\", i)) == std::string::npos )
+			break ;
+
+		c = str.at(i + 1);
+		str = str.erase( i, 2 );
+		if ( c == 'a' )
+			str = str.insert( i, "\a" );
+		else if ( c == 'b' )
+			str = str.insert( i, "\b" );
+		else if ( c == 'f' )
+			str = str.insert( i, "\f" );
+		else if ( c == 'n' )
+			str = str.insert( i, "\n" );
+		else if ( c == 'r' )
+			str = str.insert( i, "\r" );
+		else if ( c == 't' )
+			str = str.insert( i, "\t" );
+		else if ( c == 'v' )
+			str = str.insert( i, "\v" );
+	}
+
+	return ( str );
+}
