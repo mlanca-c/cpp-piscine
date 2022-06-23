fe7c8406caeee80719aa286f627eb53151c304bf
mlanca-c <marialsottomayor@gmail.com>
2022-06-23 12:08:20 +0100

    ex00 Done in a c++ manner

--- cpp-module-00/ex00/src/megaphone.cpp
+++ cpp-module-00/ex00/src/megaphone.cpp
@@ -6,27 +6,30 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 12:39:50 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/06/20 21:50:00 by mlancac          ###   ########.fr       */
+/*   Updated: 2022/06/23 10:30:17 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
 #include <string.h>
 
 int	main(int argc, char **argv) {
 
-	if (argc == 1) {
+	std::string	str;
+
+	if ( argc == 1 ) {
 		std::cout << "* LOUD AND UNBERABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 
-	for (int i = 1; i < argc; i++) {
-		for (int j = 0; j < (int)strlen(argv[i]); j++) {
-			std::cout << (char)toupper(argv[i][j]);
-		}
+	for ( int i = 1; i < argc; i++ ) {
+		str = std::string(argv[i]);
+
+		for ( int j = 0; j < ( int )str.length(); j++ )
+			std::cout << ( char )std::toupper(str[j]);
 		std::cout << " ";
 	}
 	std::cout << std::endl;
-	return (0);
+
+	return ( 0 );
 }
