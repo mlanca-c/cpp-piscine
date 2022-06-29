1fc3721302315762d9a0e9d97b42e33ef14460fd
mlanca-c <marialsottomayor@gmail.com>
2022-06-29 11:13:34 +0100

    ex01 done

--- cpp-module-01/ex01/src/main.cpp
+++ cpp-module-01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 15:43:48 by mlanca-c          #+#    #+#             */
-/*   Updated: 2022/06/26 14:50:42 by mlancac          ###   ########.fr       */
+/*   Updated: 2022/06/28 15:39:21 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,17 @@ int	main( int argc, char *argv[] ) {
 	( void )argc;
 	( void )argv;
 
+	std::string	name;
+
+	if ( argc == 2 )
+		name = std::string( argv[1] );
+	else
+		name = "z";
 	Zombie*		horde;
 
 	{
 		LOG( "test 1: 1 Zombie" );
-		horde = zombieHorde( 1, "z" );
+		horde = zombieHorde( 1, name );
 
 		for ( int i = 0; i < 1; i++ ) {
 			horde[i].anounce();
@@ -32,7 +38,7 @@ int	main( int argc, char *argv[] ) {
 	}
 	{
 		LOG( "test 2: 42 Zombies" );
-		horde = zombieHorde( 42, "z" );
+		horde = zombieHorde( 42, name );
 
 		for ( int i = 0; i < 42; i++ ) {
 			horde[i].anounce();
@@ -42,13 +48,13 @@ int	main( int argc, char *argv[] ) {
 	}
 	{
 		LOG( "test 3: 0 Zombies" );
-		horde = zombieHorde( 0, "z" );
+		horde = zombieHorde( 0, name );
 
 		delete [] horde;
 	}
 	{
 		LOG( "test 4: -2 Zombies" );
-		horde = zombieHorde( -2, "z" );
+		horde = zombieHorde( -2, name );
 
 		delete [] horde;
 	}
