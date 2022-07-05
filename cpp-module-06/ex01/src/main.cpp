37ce070a9b821528507a0ba5f7027862a89d4fad
mlanca-c <marialsottomayor@gmail.com>
2022-07-05 21:02:02 +0100

    ex01 done

--- cpp-module-06/ex01/src/main.cpp
+++ cpp-module-06/ex01/src/main.cpp
@@ -6,24 +6,58 @@
 /*   By: mlancac </var/spool/mail/mlancac>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 16:26:29 by mlancac           #+#    #+#             */
-/*   Updated: 2022/07/05 16:34:56 by mlancac          ###   ########.fr       */
+/*   Updated: 2022/07/05 21:01:50 by mlancac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sstream>
+#include <stdint.h>
 #include "Debug.hpp"
 
-uintptr_t	serialize( Data* ptr );
-Data*		deserialize( uintptr_t raw );
+struct	Data {
+	int		i;
+	float	f;
+	double	d;
+};
+
+uintptr_t	serialize( Data* ptr ) {
+	return ( reinterpret_cast<uintptr_t>( ptr ));
+}
+
+Data*		deserialize( uintptr_t raw ) {
+	return ( reinterpret_cast<Data*>( raw ));
+}
 
 int	main( int argc, char *argv[] ) {
 
 	( void )argc;
 	( void )argv;
 
-	{
-		LOG( "test 1: " );
+	std::string			str;
+	std::stringstream	ss;
+
+	Data	d1 = { 42, 4.2f, -4.2 };
+
+	LOG( "test 1: serialization test" );
+	Data*		d2 = NULL;
+	uintptr_t	ptr;
+	( void )ptr;
+
+	ptr = serialize( &d1 );
+	d2 = deserialize( ptr );
+	if ( &d1 != d2 ) {
+
+		ERROR( &d1 << " is different than " << d2 );
+		return ( 1 );
 	}
+	std::cout << &d1 << " is equal to " << d2 << std::endl;
+
+	std::cout << "d1: { "
+		<< d1.i << ", " << d1.f << ", " << d1.d << " }" << std::endl;
+	std::cout << "d2: { "
+		<< d2->i << ", " << d2->f << ", " << d2->d << " }" << std::endl;
+
 
 	return ( 0 );
 }
